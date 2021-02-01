28aaf6db72b52a4bb6f4aecdfd10dacbac7f34e2
neerajjh <neerajjh@NEERAJJH-IN.in.oracle.com>
2021-02-01 11:44:53 +0530

    prod ci-cd setup

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -2,16 +2,14 @@ const { merge } = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const commonConfig = require('./webpack.common')
-
 const packageJson = require('../package.json')
 
-const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8081,
-    historyApiFallback: {
-      index: 'index.html'
-    }
+const domain = process.env.PRODUCTION_DOMAIN
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -25,4 +23,4 @@ const devConfig = {
   ]
 }
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, prodConfig)
