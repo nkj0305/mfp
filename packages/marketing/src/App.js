2ab5a7f53c246b5d8bb4d42751bdc2684da17bbc
neerajjh <neerajjh@NEERAJJH-IN.in.oracle.com>
2021-02-01 15:18:43 +0530

    added cloudfront invalidation for marketing with a small fix

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
 export default () => {
+  console.log('marketing')
   return (
     <div>
       <StylesProvider>
